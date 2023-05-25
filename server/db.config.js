082a955700ea5643337ae8673f8229449e5d6e74
Nikhil Kamble <nikhiil.kamble08@gmail.com>
2023-05-24 21:17:07 -0500

    added db connection parameters

--- server/db.config.js
+++ server/db.config.js
@@ -2,5 +2,5 @@ const dbName = "Orderly";
 const dbHost = "127.0.0.1";
 const dbPort = 27017;
 module.exports = {
-  url: `mongodb://${dbHost}:${dbPort}/${dbName}`,
+  url: `mongodb+srv://root:root@cluster0.ctabcdm.mongodb.net/Orderly`,
 };
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://root:root@cluster0.ctabcdm.mongodb.net/?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect("mongodb+srv://root:root@cluster0.ctabcdm.mongodb.net/Orderly", {
+    useNewUrlParser: true,
+  })
   .catch((e) => {
     console.error("connection error", e.message);
   });
